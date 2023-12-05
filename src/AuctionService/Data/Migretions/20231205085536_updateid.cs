6bd4ab4fbb7d1fc778fcb2694e07db546d2d0683
Tomer Kedem <tompha1@hotmail.com>
2023-12-05 21:47:02 +0200

    End of section 2

---
+++ src/AuctionService/Data/Migretions/20231205085536_updateid.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migretions
+{
+    /// <inheritdoc />
+    public partial class updateid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Auctions",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Auctions",
+                newName: "id");
+        }
+    }
+}
